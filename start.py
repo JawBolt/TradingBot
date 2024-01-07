64c426cbbc38867017138bdce5a03fd336eca07f
JawBolt <139167368+JawBolt@users.noreply.github.com>
2024-01-07 00:29:46 +0000

    Create start.py

---
+++ start.py
@@ -0,0 +1,26 @@
+
+import user_data as UD
+import trading
+
+print(" ")
+print("Crypto Trading bot V1")
+print("By JawBolt (George)")
+print(" ")
+api_key = input("Enter your API Key: " )
+api_secret = input("Enter your Secret Key: ")
+try:
+    UD.user_balence(str(api_key), str(api_secret))
+    print(" ")
+    print("Succesful login!")
+    print(" ")
+    print("Your current Balence is", UD.user_balence(str(api_key), str(api_secret)))
+    print(" ")
+    print("Starting...")
+    trading.main(str(api_key), str(api_secret))
+    
+
+except:
+    print(" ")
+    print("Invalid keys or unable to connect!")
+    print("Exiting...")
+    exit()
