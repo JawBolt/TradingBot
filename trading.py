dcaf97e0c7969c5ef27984dd929cdccc9c566cbf
JawBolt <139167368+JawBolt@users.noreply.github.com>
2024-01-12 11:08:12 +0000

    Update trading.py

--- trading.py
+++ trading.py
@@ -1,1 +1,27 @@
-import user_data
+
+import user_data as UD
+import market_data as ms
+import buyandsell
+import math
+import json
+
+running = True
+
+# Algo
+# check if a bullish candle patturn
+# caculate multiple supports and resists (2 peaks within +- 50)
+# if all are met buy
+
+class create_candle:
+  def __init__(self, raw_candle):
+    self.self = self
+    self.raw_candle = raw_candle
+    
+  def get_open(self):
+    
+    
+
+def main(API_KEY, SECRET_KEY):
+  while running == True:
+    
+    
