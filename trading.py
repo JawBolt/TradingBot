e30ea4732d3a70292d24d5af405f70154b55fcf1
JawBolt <139167368+JawBolt@users.noreply.github.com>
2024-02-15 00:32:33 +0000

    more changes

--- trading.py
+++ trading.py
@@ -5,14 +5,36 @@ import buyandsell
 import math
 import json
 
-class create_candle:
-    def __init__(self, raw_candle, idNumb):
-        self.self = self
-        self.raw_candle = raw_candle
-        self.idNumb = idNumb
+coin = ""
+config = open("config.txt", "r")
+content = config.readlines()
+if content[0] == "COIN=SOL":
+    coin = "SOL"
+running = True
 
-    def get_id(self):
-        return str(self.idNumb)
-    
-    def get_open(self):
-        
+
+
+def main(api_key, api_secret):
+    print(" ")
+    print("All bot activity will be displayed here")
+    print(" ")
+    print("Type help for commands.")
+    print(" ")
+
+    while running == True:
+        cmd = input("> ")
+
+        if cmd == "help":
+            print("bal --> returns your balence")
+            print("orders --> returns all open orders")
+            print("stop --> stops the bot temporarily")
+            print("coin --> returns what coin is being traded")
+            print("profit --> shows profit/loss from when the bot started until now")
+            print("shutdown --> exits the program")
+
+        if cmd == "bal":
+            print("Your current main balence is: " + UD.user_balence(str(api_key), str(api_secret)))
+
+        if cmd == "coin":
+            print("The currently traded coin is: " + )
+
